cd2d69e3c19d1860aa4b0ba4991d289f17fa537e
Administrator <Administrator@PC207>
2020-06-08 23:00:30 +0700

    Tuan 4

--- Lap456/Controllers/HomeController.cs
+++ Lap456/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using Lap456.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+
 
 namespace Lap456.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext(); 
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses.Include(c => c.Lecture).Include(c => c.Categlory).Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
